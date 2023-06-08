a50aa847d371b846b4647bc28aa3274e6ea42939
vicoda <54949883+vicoda@users.noreply.github.com>
2023-06-08 15:48:15 -0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+UserSubreddit = "wallstreetbets"
+#other subreddits [wallstreetbets, BotsPlayHere]
+
+LoopCount = 0           #Loop count for the main file might be useful later or for debugging
+SubmissionLimit = 5     #Submussion limit for every subreddit in RedditFiles    
+CommentLimit = 10       #Comment limit to get from every submission in RedditFiles
+                        #Warning, sorting has not been done yet
